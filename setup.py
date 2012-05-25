d994be7a9c27fd74e031469e1c633d6aeed76213
Muhammad M Rahman <mmrs151@gmail.com>
2012-05-25 08:41:52 +0100

    Rename resources

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
      description='Return city, country, location, business details of given/outgoing ip',
      author='Muhammad M Rahman',
      author_email='mmrs151@gmail.com',
-     url='https://github.com/mrahma01/ip2location',
+     url='https://github.com/mrahma01/iplocation',
      py_modules=['iplocation'],
      classifiers=[
           "License :: OSI Approved :: GNU General Public License (GPL)",
